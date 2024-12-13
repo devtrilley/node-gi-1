e5a59c22deb67bad2b4015c0244a1ed3e9203a59
Tamsir Rilley <tamsirrilley@gmail.com>
2024-12-13 16:22:51 -0500

    Initial commit

---
+++ deliverable-1/script.js
@@ -0,0 +1,15 @@
+/*
+Deliverable 1: Language Detection Code
+
+Find an npm package that tells you the language a string is written in.
+Print the most accurate language for the following phrases:
+
+“Es macht gut”
+“Dobrá práce”
+“Gwaith da”
+
+Push code to GitHub repository called “Language Detection Code”
+
+Take screenshot of results and push them to repo as well
+*/
+
---
+++ deliverable-2/script.js
@@ -0,0 +1,9 @@
+/*
+Deliverable 2: Note Taking App
+
+Code-along to create Andrew Mead’s note taking application.
+
+Add an edit command to the application that requires a title and note text. It should look for a note with the title submitted and update it with the new text.
+
+Push the files necessary to spin up your application to a GitHub repository called “Note Taking App.”
+*/
